39c2db83ddae5576fe1b335ab55169b638fe5132
William DeMeo <williamdemeo@gmail.com>
2013-12-15 17:17:36 -0500

    added exercise from end of Lecture 4.1

--- progfun/src/week4/List.scala
+++ progfun/src/week4/List.scala
@@ -1,4 +1,3 @@
-// import java.util.NoSuchElementException
 package week4
 
 // Lecture 3.3 -- Polymorphism
@@ -23,3 +22,9 @@ class Nil[T] extends List[T] {
   
 }
 
+object List {
+  // List(1, 2) = List.apply(1, 2)
+  def apply[T](x1: T, x2: T): List[T] = new Cons(x1, new Cons(x2, new Nil))
+  def apply[T]() = new Nil
+  
+}
